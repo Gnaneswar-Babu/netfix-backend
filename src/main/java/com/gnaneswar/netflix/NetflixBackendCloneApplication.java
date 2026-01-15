003754488ecc09b636cf5c4f4d7a826b3b35852b
Gnaneswar Babu <gnaneswarbabu95@gmail.com>
2026-01-15 13:19:45 +0530

    Initial commit-Netflix backend Spring Boot project

---
+++ src/main/java/com/gnaneswar/netflix/NetflixBackendCloneApplication.java
@@ -0,0 +1,13 @@
+package com.gnaneswar.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixBackendCloneApplication {
+
+	public static void main(String[] args)
+	{SpringApplication.run(NetflixBackendCloneApplication.class, args);
+	}
+
+}
